ee7c92bcb8d4037fb9baaf376fd694aeb9e48413
Georgy Komkoc <komkov.g.o@yandex.com>
2024-04-04 15:48:32 +0300

    refactoring

--- src/components/cards/CardHeader.jsx
+++ src/components/cards/CardHeader.jsx
@@ -1,23 +1,24 @@
 import { observer } from 'mobx-react-lite';
 
-const CardHeader = observer(({ cardInfo }) => (
-  <div className="card-header">
-    <p style={
-          { color: cardInfo.mobileAppDashboard.highlightTextColor }
-        }
-    >
-      {cardInfo.mobileAppDashboard.companyName}
-    </p>
-    <img
-      style={{
-        display: 'inline-block',
-        padding: '5px',
-        borderRadius: '50%',
-      }}
-      src={cardInfo.mobileAppDashboard.logo}
-      alt="Логотип"
-    />
-  </div>
-));
+const CardHeader = observer(({ cardInfo }) => {
+  const { mobileAppDashboard } = cardInfo;
+
+  return (
+    <div className="card-header">
+      <p style={{ color: mobileAppDashboard.highlightTextColor }}>
+        {mobileAppDashboard.companyName}
+      </p>
+      <img
+        style={{
+          display: 'inline-block',
+          padding: '5px',
+          borderRadius: '50%',
+        }}
+        src={mobileAppDashboard.logo}
+        alt="Логотип"
+      />
+    </div>
+  );
+});
 
 export default CardHeader;
